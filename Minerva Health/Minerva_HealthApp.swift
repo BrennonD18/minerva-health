1bef62c4819d0e8af0e9317a4b6e2ceef2bf7a0e
Brennon Doman <btanner1818@gmail.com>
2026-02-16 18:21:08 -0500

    Initial Commit

---
+++ Minerva Health/Minerva_HealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  Minerva_HealthApp.swift
+//  Minerva Health
+//
+//  Created by Brennon Doman on 2026-02-16.
+//
+
+import SwiftUI
+
+@main
+struct Minerva_HealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
